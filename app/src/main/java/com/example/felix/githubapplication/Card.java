e7acf6640255da5837a507ed0df05c988f69a687
belletarde <belletarde@gmail.com>
2017-07-10 20:56:10 -0300

    Commitando.

--- app/src/main/java/com/example/felix/githubapplication/Card.java
+++ app/src/main/java/com/example/felix/githubapplication/Card.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-//----//
+
 class Card {
     private Activity act;
     private ViewGroup eachItemGitHub;
